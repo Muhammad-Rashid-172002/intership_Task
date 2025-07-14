5d0c8dc3ed71fca1f3f7324792fe471d16bcde6f
Muhammad Rashid <muhammadrashid172002@gmail.com>
2025-07-14 03:11:35 -0700

    Multi Fruit and veg scanning displaying result

--- lib/views/screen/home/Screen/homescreen.dart
+++ lib/views/screen/home/Screen/homescreen.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:intership_task/views/screen/home/Screen/stripe_payment/stripe_payment.dart';
+import 'package:intership_task/views/screen/home/fruit_veg_data/data.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -15,21 +13,12 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   File? _image;
-  String? _detectedFruit;
+  String? _detectedItem;
   bool _isLoading = false;
   final ImagePicker _picker = ImagePicker();
+  final Map<String, String> _nutritionData = {};
 
   late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
-
-  final Map<String, Map<String, String>> nutritionInfo = {
-    "carrot": {
-      "RS": "100",
-      "Vitamin B": "214% of the RDI",
-      "Calories": "41",
-      "Vitamin A": "334% of the RDI",
-      "Sugar": "4.7g",
-    },
-  };
 
   @override
   void initState() {
@@ -37,22 +26,26 @@ class _HomescreenState extends State<Homescreen> {
     _setupNotifications();
   }
 
-  void _setupNotifications() async {
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-    await messaging.requestPermission();
-
+  Future<void> _setupNotifications() async {
     flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-    const AndroidInitializationSettings androidSettings =
-        AndroidInitializationSettings('assets/app_icon/unnamed.png');
-
-    const InitializationSettings initSettings = InitializationSettings(
-      android: androidSettings,
+    const androidSettings = AndroidInitializationSettings(
+      '@mipmap/ic_launcher',
     );
-
+    const initSettings = InitializationSettings(android: androidSettings);
     await flutterLocalNotificationsPlugin.initialize(initSettings);
 
-    print('Notifications initialized');
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'default_channel',
+      'Default Channel',
+      importance: Importance.high,
+    );
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin
+        >()
+        ?.createNotificationChannel(channel);
   }
 
   Future<void> _pickImage() async {
@@ -60,319 +53,94 @@ class _HomescreenState extends State<Homescreen> {
 
     try {
       final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+      if (pickedFile == null) return;
 
-      if (pickedFile != null) {
-        setState(() {
-          _image = File(pickedFile.path);
-          _detectedFruit = "carrot"; // Simulated detection
-        });
+      _image = File(pickedFile.path);
+      _detectedItem = detectFruitFromImage(_image!);
 
-        print('Image picked: ${pickedFile.path}');
-        print('Detected fruit: carrot');
+      setState(() {
+        _nutritionData.clear();
+        _nutritionData.addAll(localNutritionData[_detectedItem!] ?? {});
+      });
 
-        flutterLocalNotificationsPlugin.show(
-          1,
-          'Scan Result',
-          'You scanned a Carrot!',
-          const NotificationDetails(
-            android: AndroidNotificationDetails(
-              'default_channel',
-              'Default',
-              importance: Importance.max,
-              priority: Priority.high,
-            ),
+      flutterLocalNotificationsPlugin.show(
+        0,
+        'Scan Result',
+        'You scanned a $_detectedItem!',
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'default_channel',
+            'Default Channel',
+            importance: Importance.max,
+            priority: Priority.high,
           ),
-        );
-      } else {
-        print('No image selected');
-      }
+        ),
+      );
     } catch (e) {
-      print('Error picking image: $e');
+      print('Image pick error: $e');
       ScaffoldMessenger.of(
         context,
-      ).showSnackBar(SnackBar(content: Text('Error picking image: $e')));
+      ).showSnackBar(SnackBar(content: Text('Error: $e')));
+    } finally {
+      setState(() => _isLoading = false);
     }
-
-    setState(() => _isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    final nutrition =
-        _detectedFruit != null ? nutritionInfo[_detectedFruit!] : null;
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('NutriScan'),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
+      appBar: AppBar(title: const Text('NutriScan')),
       floatingActionButton: FloatingActionButton(
         onPressed: _isLoading ? null : _pickImage,
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        tooltip: 'Pick Image',
-        heroTag: 'pickImageButton',
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         child:
             _isLoading
-                ? const SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: SpinKitFadingCircle(color: Colors.white, size: 24.0),
-                )
+                ? const CircularProgressIndicator(color: Colors.white)
                 : const Icon(Icons.camera_alt),
       ),
-
-      body: Column(
-        children: [
-          const SizedBox(height: 16),
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 7,
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              itemBuilder: (context, index) {
-                final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-                final today = DateTime.now().weekday; // 1 = Mon, ..., 7 = Sun
-                final isToday = (index + 1) == today;
-
-                return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 6),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: isToday ? Colors.blue : Colors.grey[200],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Center(
-                    child: Text(
-                      days[index],
-                      style: TextStyle(
-                        color: isToday ? Colors.white : Colors.black87,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  margin: const EdgeInsets.all(16),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Text(
-                          "3000",
-                          style: TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        const Text(
-                          "Calories Remaining",
-                          style: TextStyle(fontSize: 14),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 12),
-                        Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            SizedBox(
-                              width: 60,
-                              height: 60,
-                              child: CircularProgressIndicator(
-                                value: 0.8,
-                                backgroundColor: Colors.grey[300],
-                                strokeWidth: 6,
-                                valueColor: const AlwaysStoppedAnimation(
-                                  Colors.black,
-                                ),
-                              ),
-                            ),
-                            const Icon(Icons.local_fire_department, size: 24),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:
+            _image == null
+                ? const Center(
+                  child: Text('Tap the camera to scan a fruit or vegetable.'),
+                )
+                : Column(
                   children: [
-                    Card(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 6,
-                        horizontal: 16,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 14,
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  '180 g',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                Text(
-                                  'Carbs left',
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                              ],
-                            ),
-                            CircleAvatar(
-                              backgroundColor: Colors.grey[200],
-                              child: Icon(Icons.rice_bowl, color: Colors.brown),
-                            ),
-                          ],
+                    Image.file(_image!, height: 200),
+                    const SizedBox(height: 20),
+                    if (_detectedItem != null)
+                      Text(
+                        'Detected: $_detectedItem',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Card(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 6,
-                        horizontal: 16,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 14,
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  '177 g',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                Text(
-                                  'Protein left',
-                                  style: TextStyle(fontSize: 14),
-                                ),
-                              ],
-                            ),
-                            CircleAvatar(
-                              backgroundColor: Colors.grey[200],
-                              child: Icon(Icons.egg, color: Colors.brown),
-                            ),
-                          ],
+                    const SizedBox(height: 10),
+                    if (_nutritionData.isNotEmpty)
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: _nutritionData.length,
+                          itemBuilder: (context, index) {
+                            final key = _nutritionData.keys.elementAt(index);
+                            return ListTile(
+                              title: Text(key),
+                              trailing: Text(_nutritionData[key]! ?? ''),
+                            );
+                          },
                         ),
                       ),
-                    ),
                   ],
                 ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16),
-          if (_image != null)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.file(_image!, height: 200),
-              ),
-            ),
-          const SizedBox(height: 16),
-          if (nutrition != null)
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Card(
-                  elevation: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: ListView.builder(
-                      itemCount: nutrition.length,
-                      itemBuilder: (context, index) {
-                        final key = nutrition.keys.elementAt(index);
-                        final value = nutrition[key];
-
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                key,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              key == "RS"
-                                  ? TextButton(
-                                    onPressed: () {
-                                      showModalBottomSheet(
-                                        context: context,
-                                        shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.vertical(
-                                            top: Radius.circular(20),
-                                          ),
-                                        ),
-                                        isScrollControlled: true,
-                                        builder:
-                                            (context) =>
-                                                const PaymentBottomSheet(),
-                                      );
-                                    },
-                                    child: Text(
-                                      value ?? '',
-                                      style: const TextStyle(fontSize: 16),
-                                    ),
-                                  )
-                                  : Text(
-                                    value ?? '',
-                                    style: const TextStyle(fontSize: 16),
-                                  ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            )
-          else
-            const Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'Capture a food image to see its nutrition information.',
-                style: TextStyle(fontSize: 16, color: Colors.black54),
-              ),
-            ),
-        ],
       ),
     );
   }
 }
+
+String detectFruitFromImage(File image) {
+  final path = image.path.toLowerCase();
+  if (path.contains("assets/assets images/images (2).jpeg")) return "apple";
+  if (path.contains("assets/assets images/images (1).jpeg")) return "banana";
+  if (path.contains("assets/assets images/images.jpeg")) return "potato";
+  return "unknown";
+} //  detection based on filename
---
+++ lib/views/screen/home/fruit_veg_data/data.dart
@@ -0,0 +1,24 @@
+// fruit_data.dart
+final Map<String, Map<String, String>> localNutritionData = {
+  "apple": {
+    "Rs": "120",
+    "Calories": "52 kcal",
+    "Vitamin C": "4.6 mg",
+    "Fiber": "2.4 g",
+    "Sugar": "10.4 g",
+  },
+  "banana": {
+    "Rs": "150",
+    "Calories": "89 kcal",
+    "Vitamin C": "8.7 mg",
+    "Fiber": "2.6 g",
+    "Sugar": "12.2 g",
+  },
+  "potato": {
+    "Rs": "100",
+    "Calories": "77 kcal",
+    "Vitamin C": "19.7 mg",
+    "Fiber": "2.2 g",
+    "Sugar": "0.8 g",
+  },
+};
